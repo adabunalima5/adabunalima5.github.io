3be414e2ee0f8f86f93f26ef1c05f0e4f12749fe
adabunalima5 <160842219+adabunalima5@users.noreply.github.com>
2025-04-06 17:40:18 +0700

    Update iklanacak.js

--- file/js/iklanacak.js
+++ file/js/iklanacak.js
@@ -1,61 +1,34 @@
 (function() {
   const adList = [
-    {
-      key: 'e57184ca54659540fc83130debe739cd',
-      width: 300,
-      height: 250
-    },
-    {
-      key: 'd79498fdb04107ec3769b0736a773697',
-      width: 728,
-      height: 90
-    },
-    {
-      key: '538a1dcc9cea9369de0ee1bcb55aa7d9',
-      width: 160,
-      height: 600
-    },
-    {
-      key: '425a5d27384cd5676f6b66378d9bef27',
-      width: 160,
-      height: 300
-    },
-    {
-      key: '267b146662dfa50c642d62afb5da540b',
-      width: 468,
-      height: 60
-    },
-    {
-      key: 'd88366ae7a148d059c98b4410168aef8',
-      width: 320,
-      height: 50
-    },
-    {
-      key: '627903e07c5a1bb9b3358c85f75744e0',
-      width: 320,
-      height: 50
-    },
-    {
-      key: '4047d36e8b585ba1ccb08c74092b269e',
-      width: 728,
-      height: 90
-    },
-    {
-      key: '8d987a2cdd1810732f9e719e8aa7b118',
-      width: 300,
-      height: 250
-    },
-    {
-      key: 'ab67ce079593c5246f7e7a60ac7ee861',
-      width: 160,
-      height: 600
-    }
+    { key: 'e57184ca54659540fc83130debe739cd', width: 300, height: 250 },
+    { key: 'd79498fdb04107ec3769b0736a773697', width: 728, height: 90 },
+    { key: '538a1dcc9cea9369de0ee1bcb55aa7d9', width: 160, height: 600 },
+    { key: '425a5d27384cd5676f6b66378d9bef27', width: 160, height: 300 },
+    { key: '267b146662dfa50c642d62afb5da540b', width: 468, height: 60 },
+    { key: 'd88366ae7a148d059c98b4410168aef8', width: 320, height: 50 },
+    { key: '627903e07c5a1bb9b3358c85f75744e0', width: 320, height: 50 },
+    { key: '4047d36e8b585ba1ccb08c74092b269e', width: 728, height: 90 },
+    { key: '8d987a2cdd1810732f9e719e8aa7b118', width: 300, height: 250 },
+    { key: 'ab67ce079593c5246f7e7a60ac7ee861', width: 160, height: 600 },
+    // kamu bisa tambahkan lebih banyak jika perlu
   ];
+
+  // Fungsi untuk mengacak array (Fisher–Yates Shuffle)
+  function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
 
   const container = document.getElementById('iklan-container');
   if (!container) return;
 
-  adList.forEach(ad => {
+  // Acak dan ambil 10 iklan
+  const randomizedAds = shuffle(adList).slice(0, 10);
+
+  randomizedAds.forEach(ad => {
     const script1 = document.createElement('script');
     script1.type = 'text/javascript';
     script1.innerHTML = `
@@ -67,6 +40,7 @@
         'params' : {}
       };
     `;
+
     const script2 = document.createElement('script');
     script2.type = 'text/javascript';
     script2.src = '//www.highperformanceformat.com/' + ad.key + '/invoke.js';
@@ -80,4 +54,3 @@
     container.appendChild(div);
   });
 })();
-
